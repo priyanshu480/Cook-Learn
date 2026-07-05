eb917aedb58555806bdd7eb5ec0bf73cf9794d4a
priyanshu jha <163561752+priyanshu480@users.noreply.github.com>
2026-07-06 00:27:39 +0530

    Update practice.cs

--- practice.cs
+++ practice.cs
@@ -1,108 +1,77 @@
-S-3 Q-2
+<h1>Task Tracker</h1>
+<app-task-input (taskAdded)="addTask($event)">
+</app-task-input>
+<app-task-list [tasks]="tasks" (taskDeleted)="deleteTask($event)">
+</app-task-list>
+<router-outlet></router-outlet>
  
+app.html
  
+<h1>Task Input</h1>
  
-<h1>Product Details</h1>
+<input type="text" placeholder="Enter Task Description" [(ngModel)]="taskInput">
+<button (click)="addTask()">Add Task</button>
  
-<div class="product-details-box" *ngIf="selectedProduct">
+task-input.html
  
-    <img [src]="selectedProduct.imageUrl" [alt]="selectedProduct.name" width="150">
- 
-    <h3>{{ selectedProduct.name }}</h3>
- 
-    <p>{{ selectedProduct.description }}</p>
- 
-    <p>Price: {{ selectedProduct.price }}</p>
- 
-</div>
- 
-import { Component, Input } from '@angular/core';
- 
-@Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
-})
-export class ProductDetailsComponent {
- 
-  @Input()
-  selectedProduct: any;
-}
- 
- 
-<h1>Product List</h1>
- 
-<div class="product-item" *ngFor="let product of products">
-    <img [src]="product.imageUrl" [alt]="product.name">
- 
-    <h3>{{ product.name }}</h3>
- 
-    <button (click)="viewDetails(product)">
-        View Details
-    </button>
- 
-</div>
- 
+import { outputAst } from '@angular/compiler';
 import { Component, EventEmitter, Output } from '@angular/core';
  
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: 'app-task-input',
+  templateUrl: './task-input.component.html',
+  styleUrls: ['./task-input.component.css']
 })
-export class ProductListComponent {
- 
+export class TaskInputComponent {
+  taskInput: string = '';
   @Output()
-  productSelected = new EventEmitter<any>();
- 
-  products = [
-    {
-      id: 1,
-      name: 'Pencil',
-      description: 'A pencil is an object that you write or draw with. It consists of a thin piece of wood with a rod of a black or coloured substance through the middle',
-      price: 10.99,
-      imageUrl: 'assets/pencil.jpg'
-    },
-    {
-      id: 2,
-      name: 'Scale',
-      description: 'A scale is a set of levels or numbers which are used in a particular system of measuring things or are used when comparing things',
-      price: 19.99,
-      imageUrl: 'assets/scale.jpg'
-    },
-    {
-      id: 3,
-      name: 'Eraser',
-      description: 'An eraser, piece of rubber or other material used to rub out marks made by ink, pencil, or chalk. The modern eraser is usually a mixture of an abrasive such as fine pumice, a rubbery matrix such as synthetic rubber or vinyl, and other ingredients.',
-      price: 29.99,
-      imageUrl: 'assets/eraser.jpg'
+  taskAdded = new EventEmitter<string>();
+  addTask(): void{
+    if(this.taskInput.trim() != ''){
+      this.taskAdded.emit(this.taskInput);
+      this.taskInput = '';
     }
-  ];
+  }
  
-  viewDetails(product: any): void {
-    this.productSelected.emit(product);
+}
+ 
+uska css
+ 
+js
+ 
+<h1>Task List</h1>
+<ul>
+    <li *ngFor="let task of tasks; let i = index">
+    {{task}}
+ 
+    <button (click)="deleteTask(i)">
+        Delete
+    </button>
+</li>
+</ul>
+ 
+task-list.html
+ 
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+ 
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent {
+  @Input()
+  tasks: string[] = [];
+  @Output()
+  taskDeleted = new EventEmitter<number>();
+ 
+  deleteTask(index: number): void{
+    this.taskDeleted.emit(index);
   }
 }
  
  
-<!-- <app-product-details></app-product-details>
-<app-product-list></app-product-list> -->
- 
-<h1>Stationery Management System</h1>
- 
-<div class="container">
- 
-  <div class="left-panel">
-    <app-product-list (productSelected)="onProductSelected($event)">
-    </app-product-list>
-  </div>
- 
-  <div class="right-panel">
-    <app-product-details [selectedProduct]="selectedProduct">
-    </app-product-details>
-  </div>
- 
-</div>
+uska .ts
  
 import { Component } from '@angular/core';
  
@@ -112,39 +81,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
   title = 'angularapp';
- 
-  selectedProduct: any;
- 
-  onProductSelected(product: any): void {
-    this.selectedProduct = product;
+  tasks: string[] = [];
+  addTask(task: string): void{
+    this.tasks.push(task);
+  }
+  deleteTask(index: number): void{
+    this.tasks.splice(index, 1);
   }
 }
  
- 
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
- 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
- 
-@NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent,
-    ProductDetailsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
- 
- 
+appcomponent.ts
  
