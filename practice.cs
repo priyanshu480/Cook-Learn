75a36c684cd892fd669aa8e76420bb9714946cc8
priyanshu jha <163561752+priyanshu480@users.noreply.github.com>
2026-07-07 22:36:10 +0530

    Update practice.cs

--- practice.cs
+++ practice.cs
@@ -1,118 +1,238 @@
-
- 
-registration-form.component.ts
+contact-form.component.ts
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+  ValidationErrors
+} from '@angular/forms';
  
 @Component({
-  selector: 'app-registration-form',
-  templateUrl: './registration-form.component.html',
-  styleUrls: ['./registration-form.component.css']
+  selector: 'app-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
 })
-export class RegistrationFormComponent {
+export class ContactFormComponent {
  
-  isSubmitted: boolean = false;
-  passwordMismatch: boolean = false;
+  contactForm: FormGroup;
  
-  onSubmit(form: NgForm): void {
+  constructor(private fb: FormBuilder) {
  
-    if (form.value.password !== form.value.confirmPassword) {
-      this.passwordMismatch = true;
-      this.isSubmitted = false;
-      return;
+    this.contactForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+ 
+    address: this.fb.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        postalCode: ['']
+      }),
+ 
+    subjectDetails: this.fb.group(
+        {
+          subject: [''],
+          message: ['']
+        },
+        {
+          validators: this.subjectMessageValidator
+        }
+      )
+    });
+  }
+ 
+  subjectMessageValidator(
+    group: AbstractControl
+  ): ValidationErrors | null {
+ 
+    const subject = group.get('subject')?.value;
+    const message = group.get('message')?.value;
+ 
+    if ((subject && !message) || (!subject && message)) {
+      return { subjectMessageRequired: true };
     }
  
-    this.passwordMismatch = false;
+    return null;
+  }
  
-    if (form.valid) {
-      this.isSubmitted = true;
-      form.resetForm();
+  onSubmit(): void {
+ 
+    if (this.contactForm.valid) {
+ 
+    console.log(this.contactForm.value);
+ 
+    this.contactForm.reset();
+ 
+    } else {
+ 
+    console.log('Form Invalid');
+ 
+    this.contactForm.markAllAsTouched();
     }
+  }
+ 
+  get firstName() {
+    return this.contactForm.get('firstName');
+  }
+ 
+  get lastName() {
+    return this.contactForm.get('lastName');
+  }
+ 
+  get email() {
+    return this.contactForm.get('email');
   }
 }
  
  
-registration-form.component.html
+contact-form.component.html
  
-<h1>Registration Form</h1>
  
-<form #registrationForm="ngForm"
-      (ngSubmit)="onSubmit(registrationForm)">
+<h1>Contact Form</h1>
  
-  <label for="name">Name</label>
-  <input
-    type="text"
-    id="name"
-    name="name"
-    ngModel
-    required>
+<form
+  [formGroup]="contactForm"
+  (ngSubmit)="onSubmit()">
  
-  <label for="email">Email</label>
-  <input
-    type="email"
-    id="email"
-    name="email"
-    ngModel
-    required
-    email>
+<div>
+    <label>First Name</label>
  
-  <label for="password">Password</label>
-  <input
-    type="password"
-    id="password"
-    name="password"
-    ngModel
-    required>
+    `<input
+      type="text"
+      formControlName="firstName">`
  
-  <label for="confirmPassword">Confirm Password</label>
-  <input
-    type="password"
-    id="confirmPassword"
-    name="confirmPassword"
-    ngModel
-    required>
+    <div
+      *ngIf="firstName?.touched && firstName?.errors?.['required']">
+      First Name is required`</div>`
  
-<div *ngIf="passwordMismatch" class="error-message">
-    Passwords do not match
-  </div>
+    <div
+      *ngIf="firstName?.touched && firstName?.errors?.['minlength']">
+      Minimum 2 characters required`</div>`
+ 
+</div>
+ 
+<br>
+ 
+<div>
+    <label>Last Name</label>
+ 
+    `<input
+      type="text"
+      formControlName="lastName">`
+ 
+    <div
+      *ngIf="lastName?.touched && lastName?.errors?.['required']">
+      Last Name is required`</div>`
+ 
+    <div
+      *ngIf="lastName?.touched && lastName?.errors?.['minlength']">
+      Minimum 2 characters required`</div>`
+ 
+</div>
+ 
+<br>
+ 
+<div>
+    <label>Email</label>
+ 
+    `<input
+      type="email"
+      formControlName="email">`
+ 
+    <div
+      *ngIf="email?.touched && email?.errors?.['required']">
+      Email is required`</div>`
+ 
+    <div
+      *ngIf="email?.touched && email?.errors?.['email']">
+      Invalid email format`</div>`
+ 
+</div>
+ 
+<br>
+ 
+<div formGroupName="address">
+ 
+    `<h3>`Address`</h3>`
+ 
+    `<input
+      type="text"
+      formControlName="street"
+      placeholder="Street">`
+ 
+    `<input
+      type="text"
+      formControlName="city"
+      placeholder="City">`
+ 
+    `<input
+      type="text"
+      formControlName="state"
+      placeholder="State">`
+ 
+    `<input
+      type="text"
+      formControlName="postalCode"
+      placeholder="Postal Code">`
+ 
+</div>
+ 
+<br>
+ 
+<div formGroupName="subjectDetails">
+ 
+    `<h3>`Subject Details`</h3>`
+ 
+    `<input
+      type="text"
+      formControlName="subject"
+      placeholder="Subject">`
+ 
+    `<textarea
+      formControlName="message"
+      placeholder="Message"></textarea>`
+ 
+    <div
+      *ngIf="contactForm.get('subjectDetails')?.errors?.['subjectMessageRequired']">
+      Subject and Message must both be filled.`</div>`
+ 
+</div>
+ 
+<br>
  
   <button
     type="submit"
-    [disabled]="registrationForm.invalid">
-    Register
-  </button>
+    [disabled]="contactForm.invalid">
+    Submit
+  `</button>`
  
 </form>
- 
-<div *ngIf="isSubmitted" class="success-message">
-  Registration successful!
-</div>
  
  
 app.module.ts
  
- 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
  
 import { AppComponent } from './app.component';
-import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
  
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationFormComponent
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
  
  
-app.component.html
-<app-registration-form></app-registration-form>
- 
+app.comonent.html
+<app-contact-form></app-contact-form>
